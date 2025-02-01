bd0a5b09974d6e91eaa0f3c5b56050972ea314f3
Akiko0210 <qea643@gmail.com>
2025-01-31 18:39:24 -0800

    remove augmented

--- linear-equation.cpp
+++ linear-equation.cpp
@@ -11,7 +11,6 @@ class Matrix {
     private: 
         int n, m;
         vector<vector<int> > mat;
-        bool is_augmented;
     public: 
         Matrix() {
             
@@ -28,7 +27,6 @@ class Matrix {
                     cin >> mat[i][j];
                 }
             }
-            // cout << "Is this augmented\n"
         }
 
         void multiply_row(vector<int>& row, int x) {
